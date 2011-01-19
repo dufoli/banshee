ff30d2d85700160052b20e0ebe92b397472c1342
Olivier Dufour <olivier.duff@gmail.com>
2011-01-19 22:04:44 +0100

    [Video] Add the regexp to found tv show

--- src/Extensions/Banshee.Video/Banshee.Video/VideoService.cs
+++ src/Extensions/Banshee.Video/Banshee.Video/VideoService.cs
@@ -24,8 +24,13 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 using System;
-using Banshee.ServiceStack;
+using System.Text.RegularExpressions;
+
+using Banshee.Collection.Database;
+using Banshee.Collection;
 using Banshee.MediaEngine;
+using Banshee.ServiceStack;
+
 
 namespace Banshee.Video
 {
@@ -34,6 +39,9 @@ namespace Banshee.Video
         public VideoService ()
         {
         }
+
+        private readonly Regex regexp = new Regex (@"(.*)[._][\[]?[s]?([0-9]+)[\]]?[._]?[\[]?[-EeXx]([0-9]+)[\]]?[._].*");
+        private readonly Regex regexp2 = new Regex (@"(.*)[._]([0-9])([0-9][0-9])[._].*");
 
         private VideoLibrarySource source;
 
@@ -50,16 +58,75 @@ namespace Banshee.Video
         void OnTracksAdded (Sources.Source sender, Sources.TrackEventArgs args)
         {
             RefreshTracks ();
+            UpdateMetadata ();
         }
 
         void RefreshTracks ()
         {
-            //TODO filter to get only track with no infos
-            //source.DatabaseTrackModel
-            //DatabaseTackInfo tra;
-            //TODO get all tracks which have not artwork/data
-            //source.DatabaseTrackModel.
-            // Use custom 
+            foreach (DatabaseTrackInfo track in DatabaseTrackInfo.Provider.FetchAllMatching ("PrimarySourceID = ? AND ExternalID = 0", source.DbId)) {
+                var video = new VideoInfo ();
+                // Temporary set title but will be updated by webservice
+                video.Title = track.TrackTitle;
+                ParseName (track, video, track.TrackTitle);
+                video.Save ();
+                track.ExternalId = video.DbId;
+                track.Save ();
+            }
+        }
+
+        public void ParseName (DatabaseTrackInfo track, VideoInfo video, string name)
+        {
+            int episode;
+            Match match = regexp.Match (name);
+            if (match.Success) {
+                track.ArtistName = match.Captures[0].Value;
+                track.AlbumTitle = String.Format ("Season {0}", match.Captures[1].Value);
+                Int32.TryParse (match.Captures[2].Value, out episode);
+                track.TrackNumber = episode;
+                track.MediaAttributes |= TrackMediaAttributes.TvShow;
+                return;
+            }
+            else {
+                match = regexp2.Match (name);
+                if (match.Success) {
+                    track.ArtistName = match.Captures[0].Value;
+                    track.AlbumTitle = String.Format ("Season {0}", match.Captures[1].Value);
+                    Int32.TryParse (match.Captures[2].Value, out episode);
+                    track.TrackNumber = episode;
+                    track.MediaAttributes |= TrackMediaAttributes.TvShow;
+                    return;
+                }
+            }
+            //by default video is movie if not tv show
+            track.MediaAttributes |= TrackMediaAttributes.Movie;
+
+            //TODO multi-part tv show
+            /*The defaults will match the following structures/file name formats
+            foo.s01e01.*
+            foo.s01.e01.*
+            foo.s01_e01.*
+            foo_[s01]_[e01]_*
+            foo.1x01.*
+            foo.101.*
+
+            Defaults for two-part TV Show Episode will match
+            foo.s01e01-02.*
+            foo_[s01]_[e01-02]_*
+            foo.1x01.1x02.*
+
+            Same with three- or four-part TV Show Episode
+            foo.s01e01-02-03-04.*
+            foo_[s01]_[e01-02-03-04]_*
+            foo.1x01.1x02.1x03.1x04.*
+            */
+
+        }
+
+        void UpdateMetadata ()
+        {
+
+            //TODO call metadate service
+            //ServiceStack.ServiceManager.JobScheduler.Add (job);
         }
 
         public void Dispose ()
