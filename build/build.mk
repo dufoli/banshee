8ec0bc42fd03c9fb865b2730ee1cb89e1bebc754
Andres G. Aragoneses <knocte@gmail.com>
2013-03-10 16:50:54 +0000

    build: refactoring to remove dupe $DLL_MAP_VERIFIER_ASSEMBLY target
    After the fix for bgo#691696 landed, the target $DLL_MAP_VERIFIER_ASSEMBLY
    is defined in two places. This commits avoids this, by making the Makefile
    from the build/ dir include the generic build.rules.mk like other Makefiles
    in the build already do.
    
    The only change in this commit that seems to not be related is the move of
    the 'if ENABLE_TESTS' block. The reason for it was to avoid this build
    failure which would happen if it's not moved:
    
      Running automake --gnu --add-missing --force --copy -Wno-portability -Wno-portability ...
      build/build.rules.mk:11: LINK must be set with `=' before using `+='
    
    No change of behaviour in this commit.

--- build/build.mk
+++ build/build.mk
@@ -1,3 +1,12 @@
 include $(top_srcdir)/build/build.environment.mk
+
+ALL_TARGETS = $(ASSEMBLY_FILE) theme-icons
+
+if ENABLE_TESTS
+    LINK += " $(NUNIT_LIBS)"
+    ENABLE_TESTS_FLAG = "-define:ENABLE_TESTS"
+endif
+
+include $(top_srcdir)/build/build.dist.mk
 include $(top_srcdir)/build/build.rules.mk
 
--- build/build.rules.mk
+++ build/build.rules.mk
@@ -1,16 +1,9 @@
-include $(top_srcdir)/build/build.dist.mk
-
 UNIQUE_FILTER_PIPE = tr [:space:] \\n | sort | uniq
 BUILD_DATA_DIR = $(top_builddir)/bin/share/$(PACKAGE)
 
 INSTALL_ICONS = $(top_srcdir)/build/private-icon-theme-installer "$(mkinstalldirs)" "$(INSTALL_DATA)"
 
 INSTALL_DIR_RESOLVED = $(firstword $(subst , $(DEFAULT_INSTALL_DIR), $(INSTALL_DIR)))
-
-if ENABLE_TESTS
-    LINK += " $(NUNIT_LIBS)"
-    ENABLE_TESTS_FLAG = "-define:ENABLE_TESTS"
-endif
 
 FILTERED_LINK = $(shell echo "$(LINK)" | $(UNIQUE_FILTER_PIPE))
 DEP_LINK = $(shell echo "$(LINK)" | $(UNIQUE_FILTER_PIPE) | sed s,-r:,,g | grep '$(top_builddir)/bin/')
@@ -19,7 +12,7 @@ DLL_MAP_VERIFIER_ASSEMBLY = $(top_srcdir)/build/dll-map-verifier.exe
 moduledir = $(INSTALL_DIR_RESOLVED)
 module_SCRIPTS = $(OUTPUT_FILES)
 
-all: $(ASSEMBLY_FILE) theme-icons
+all: $(ALL_TARGETS)
 
 run:
 	@pushd $(top_builddir); \
