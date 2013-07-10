95a3535b838c5302a955d5255df3113d1c1dbc0b
Andrés G. Aragoneses <knocte@gmail.com>
2013-07-10 02:22:23 +0200

    build: do not run post-build.bat if msbuild fails (windows)
    The msbuild call is now chained to the call to post-build.bat with
    the '&&' and operator, so if the first element fails, all the rest
    is not executed.

--- build/windows/build-banshee.bat
+++ build/windows/build-banshee.bat
@@ -1,17 +1,23 @@
 @ECHO OFF
 SET v40-30319="%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild"
 SET v35="%WINDIR%\Microsoft.NET\Framework\v3.5\msbuild"
+
 CD ..\..
 ECHO "Looking for Microsoft.NET MSBuild..."
 IF EXIST %v40-30319% (
-ECHO "Building with Microsoft.NET v4.0 MSBuild"
-%v40-30319% Banshee.sln /p:Configuration=Windows /p:Platform="Any CPU"
+	ECHO "Building with Microsoft.NET v4.0 MSBuild"
+	SET MSBUILD_PATH=%v40-30319%
 ) ELSE IF EXIST "%v35%" (
-ECHO "Building with Microsoft.NET v3.5 MSBuild"
-%v35% Banshee.sln /p:Configuration=Windows /p:Platform="Any CPU"
+	ECHO "Building with Microsoft.NET v3.5 MSBuild"
+	SET MSBUILD_PATH=%v35%
 ) ELSE (
-ECHO "Build failed: Microsoft.NET MSBuild (msbuild.exe) not found"
+	ECHO "Build failed: Microsoft.NET MSBuild (msbuild.exe) not found"
+	GOTO END
 )
-ECHO 'Running "post-build.bat"'
-build\windows\post-build.bat
+
+%MSBUILD_PATH% Banshee.sln /p:Configuration=Windows /p:Platform="Any CPU" && ^
+ECHO 'Running "post-build.bat"' && ^
+build\windows\post-build.bat && ^
 CD build\windows
+
+:END
